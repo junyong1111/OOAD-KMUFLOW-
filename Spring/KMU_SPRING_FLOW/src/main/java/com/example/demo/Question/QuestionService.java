42141df1dcd1919d7e5d91b9cb174fd935175d88
Junyong <jypark93@kookmin.ac.kr>
2023-05-12 18:53:11 +0900

    add:스프링부트_부트스트랩 적용 완료

--- Spring/KMU_SPRING_FLOW/src/main/java/com/example/demo/Question/QuestionService.java
+++ Spring/KMU_SPRING_FLOW/src/main/java/com/example/demo/Question/QuestionService.java
@@ -1,9 +1,9 @@
 package com.example.demo.Question;
 
 import java.util.List;
-
+import java.util.Optional;
+import com.example.demo.DataNotFoundException;
 import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,5 +14,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
