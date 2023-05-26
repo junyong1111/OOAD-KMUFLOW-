b31499da9a12efa44569933127f84c11252529b5
Junyong <jypark93@kookmin.ac.kr>
2023-05-27 01:21:16 +0900

    add: kmu flow 부트스트랩 적용 완료

---
+++ kmuflow/src/main/java/com/project/kmuflow/Answer/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.project.kmuflow.Answer;
+
+import com.project.kmuflow.Answer.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ kmuflow/src/main/java/com/project/kmuflow/MainController.java
@@ -0,0 +1,20 @@
+package com.project.kmuflow;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/kst")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 스프링부트 KST Main page입니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
