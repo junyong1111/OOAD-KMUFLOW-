c8ab53a069a801ae0a99d7c120b472c02cab3859
Junyong <jypark93@kookmin.ac.kr>
2023-05-27 00:47:32 +0900

    remove : 스프링부트 에러로 인해 삭제 후 재설치

--- Spring/KMU_SPRING_FLOW/src/main/java/com/example/demo/Question/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Question;
-
-import com.example.demo.Question.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
-}
